c837a8fab14002f961c98c0aaa8dd8f1940327f5
taipaise <taipaise@gmail.com>
2024-03-14 20:44:04 +0900

    [Feat] Alert Builder 구현

---
+++ Gangyebab/Gangyebab/Alert/AlertBuilder.swift
@@ -0,0 +1,29 @@
+//
+//  AlertBuilder.swift
+//  Gangyebab
+//
+//  Created by 이동현 on 3/14/24.
+//
+
+import UIKit
+
+final class AlertBuilder {
+    private let alertViewController = CustomAlertViewController()
+        
+    init(
+        message: String,
+        confirmAction: CustomAlertAction,
+        isCancelNeeded: Bool
+    ) {
+        alertViewController.configure(CustomAlertViewModel(
+            message: message,
+            confirmAction: confirmAction,
+            isCancelNeeded: isCancelNeeded
+        ))
+    }
+    
+    func show(_ viewController: UIViewController) {
+        alertViewController.modalPresentationStyle = .overFullScreen
+        viewController.present(alertViewController, animated: false)
+    }
+}
