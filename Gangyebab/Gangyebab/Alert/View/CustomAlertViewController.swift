06d5bb37ee53ccc5b133767ec9ac8f5d861ac047
taipaise <taipaise@gmail.com>
2024-03-14 19:29:56 +0900

    [Design] custom alert UI 구현

---
+++ Gangyebab/Gangyebab/Alert/View/CustomAlertViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CustomAlertViewController.swift
+//  Gangyebab
+//
+//  Created by 이동현 on 3/14/24.
+//
+
+import UIKit
+
+final class CustomAlertViewController: UIViewController {
+
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var messageLabel: UILabel!
+    @IBOutlet private weak var cancelButton: UIButton!
+    @IBOutlet private weak var deleteButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
