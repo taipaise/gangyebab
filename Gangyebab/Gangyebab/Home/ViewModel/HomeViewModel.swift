b1e995151490b2bcbb2eb3ee5acc3997c1e8fef3
taipaise <83569908+taipaise@users.noreply.github.com>
2024-03-17 21:45:03 +0900

    [Feat] 날짜 변경 기능 추가 (#35)

--- Gangyebab/Gangyebab/Home/ViewModel/HomeViewModel.swift
+++ Gangyebab/Gangyebab/Home/ViewModel/HomeViewModel.swift
@@ -17,13 +17,19 @@ final class HomeViewModel: ViewModel {
         case addTodo(_ todo: TodoModel)
         case updateTodo(_ todo: TodoModel)
         case deleteTodo(_ indexPath: IndexPath)
+        case todayButtonTapped
+        case dateSelected(_ date: Date)
+        case calendarSwipe(_ date: Date)
     }
     
-    private(set) var homeType = CurrentValueSubject<HomeType,Never>(.day)
+    @Published private(set) var isToday = true
+    private(set) var homeType = CurrentValueSubject<HomeType, Never>(.day)
+    private(set) var dateString = CurrentValueSubject<String, Never>("")
+    private(set) var monthString = CurrentValueSubject<String, Never>("")
     private(set) var cellModels = CurrentValueSubject<TodoCellModels, Error>(TodoCellModels(inProgress: [], completed: []))
     private(set) var inprogressCellModels: [TodoModel] = []
     private var completedCellModels: [TodoModel] = []
-    private(set) var date = Date()
+    private(set) var date = CurrentValueSubject<Date, Never>(Date())
     private var cancellables: Set<AnyCancellable> = []
     private let dbManager = DBManager.shared
     private let dateManager = DateManager.shared
@@ -35,20 +41,39 @@ final class HomeViewModel: ViewModel {
         case .toggleComplete(let indexPath):
             toggleComplete(indexPath)
         case .nextButton:
-            print()
+            goNextDay()
         case .previousButton:
-            print()
+            goPrevioudDay()
         case .addTodo(let todo):
             addTodo(todo)
         case .deleteTodo(let indexPath):
             deleteTodo(indexPath)
         case .updateTodo(let todo):
             updateTodo(todo)
+        case .todayButtonTapped:
+            setDate(Date())
+        case .dateSelected(let date):
+            setDate(date)
+        case .calendarSwipe(let date):
+            calendarSwipe(date)
         }
     }
     
     init() {
         fetchTodoList(Date())
+        binding()
+        monthString.send(dateManager.dateToStringMonth(Date()))
+    }
+    
+    private func binding() {
+        let todayString = dateManager.dateToStringForHome(Date())
+        
+        date.sink { [weak self] date in
+            guard let dateString = self?.dateManager.dateToStringForHome(date) else { return }
+            self?.dateString.send(dateString)
+            self?.isToday = dateString == todayString
+            self?.fetchTodoList(date)
+        }.store(in: &cancellables)
     }
 }
 
@@ -77,13 +102,13 @@ extension HomeViewModel {
     
     private func addTodo(_ todo: TodoModel) {
         if dbManager.insertTodoData(todo) {
-            fetchTodoList(date)
+            fetchTodoList(date.value)
         }
     }
     
     private func updateTodo(_ todo: TodoModel) {
         if dbManager.updateTodoData(todo) {
-            fetchTodoList(date)
+            fetchTodoList(date.value)
         }
     }
     
@@ -96,7 +121,45 @@ extension HomeViewModel {
             item = completedCellModels[indexPath.row]
         }
         dbManager.deleteTodoData(item.uuid)
-        fetchTodoList(date)
+        fetchTodoList(date.value)
+    }
+    
+    private func goNextDay() {
+        switch homeType.value {
+        case .day:
+            let calendar = Calendar.current
+            var dateComponents = DateComponents()
+            dateComponents.day = 1
+            
+            if let nextDay = calendar.date(byAdding: dateComponents, to: date.value) {
+                date.send(nextDay)
+            }
+        case .month:
+            print()
+        }
+    }
+    
+    private func goPrevioudDay() {
+        switch homeType.value {
+        case .day:
+            let calendar = Calendar.current
+            var dateComponents = DateComponents()
+            dateComponents.day = -1
+            
+            if let previousDay = calendar.date(byAdding: dateComponents, to: date.value) {
+                date.send(previousDay)
+            }
+        case .month:
+            print()
+        }
+    }
+    
+    private func setDate(_ date: Date) {
+        self.date.send(date)
+    }
+    
+    private func calendarSwipe(_ date: Date) {
+        monthString.send(dateManager.dateToStringMonth(date))
     }
 }
 
