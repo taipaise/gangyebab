86edd0ab75b382962e1356ea05cbbc1ae1b45e80
taipaise <83569908+taipaise@users.noreply.github.com>
2024-03-22 16:39:03 +0900

    [Design] 후원 화면 UI 구현 (#50)

---
+++ Gangyebab/Donate/View/DonateViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DonateViewController.swift
+//  Gangyebab
+//
+//  Created by 이동현 on 3/22/24.
+//
+
+import UIKit
+
+final class DonateViewController: UIViewController {
+
+    @IBOutlet private weak var donate3000Button: UIButton!
+    @IBOutlet private weak var donate5000Button: UIButton!
+    @IBOutlet private weak var donate10000button: UIButton!
+    @IBOutlet private weak var donte15000button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
